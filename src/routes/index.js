b3eb4eeaebb5789a6dc2bfd4d29c9153e6f1bc12
Jhon Anderson Garcia <garciacalvojhonanderson@gmail.com>
2024-10-17 19:39:00 -0500

    creacion del crud estudiantes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import teacherRoutes from "./teachers.routes.js";
+import students from "./students.routes.js";
 
 const router = express.Router();
 
 router.use("/teachers", teacherRoutes);
+router.use("/students", students);
 
 export default router;
