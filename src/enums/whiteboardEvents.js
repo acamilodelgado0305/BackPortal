84d3ed9d8fa7474b76c5b9f374fda312a1121d51
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-01 10:27:17 -0300

    Mayoria de los enums funcionando

---
+++ src/enums/whiteboardEvents.js
@@ -0,0 +1,10 @@
+// whiteboardEvents.js
+const events = { 
+    CONNECTION: 'connection',
+    DISCONNECT: 'disconnect',
+    JOIN_ROOM: 'joinRoom',
+    AUDIOFILE_OPENED: 'audioFileOpened',
+  };
+  
+  export default events;
+  
--- src/sockets/whiteboardNamespace.js
+++ src/sockets/whiteboardNamespace.js
@@ -1,25 +1,33 @@
-const initWhiteboard = (io) => {
-    const whiteboardNamespace = io.of("/whiteboard");
-  
+// whiteboardNamespace.js
+import events from "../enums/whiteboardEvents.js";
 
-    whiteboardNamespace.on("connection", (socket) => {
-      console.log("Nuevo cliente conectado a la pizarra");
-  
-      socket.on("audioFileOpened", (file) => {
-        console.log("FILE:" + JSON.stringify(file.name));
-        whiteboardNamespace.emit("audioFileOpened", file);
-      });
-  
-      socket.on("clear", () => {
-        console.log("Pizarra limpiada");
-        whiteboardNamespace.emit("clear");
-      });
-  
-      socket.on("disconnect", () => {
-        console.log("Cliente desconectado de la pizarra");
-      });
+const initWhiteboard = (io) => {
+  const whiteboardNamespace = io.of("/whiteboard");
+
+  whiteboardNamespace.on(events.CONNECTION, (socket) => {
+    console.log("Nuevo cliente conectado a la pizarra");
+
+    socket.on(events.JOIN_ROOM, (room) => {
+      console.log(room);
+      socket.join(room);
     });
-  };
-  
-  export default initWhiteboard;
-  
+
+    socket.on(events.AUDIOFILE_OPENED, (fileData) => {
+      console.log("FILE:" + JSON.stringify(fileData.name));
+      const { room } = fileData;
+      socket.emit(events.AUDIOFILE_OPENED, fileData);
+      socket.to(room).emit(events.AUDIOFILE_OPENED, fileData); 
+    });
+
+    socket.on("clear", () => {
+      console.log("Pizarra limpiada");
+      whiteboardNamespace.emit("clear");
+    });
+
+    socket.on(events.DISCONNECT, () => {
+      console.log("Cliente desconectado de la pizarra");
+    });
+  });
+};
+
+export default initWhiteboard;
