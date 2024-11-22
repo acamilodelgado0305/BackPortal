213348cbcf14e3249040119d7d79e710fcbd860d
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-22 10:21:40 -0300

    Chat terminado

--- src/controllers/chatStandardSocketControlller.js
+++ src/controllers/chatStandardSocketControlller.js
@@ -5,13 +5,11 @@ import { events } from "../enums/standardChatsEvents.js";
 export const handleJoinChat = (socket) => {
     socket.on(events.JOIN_CHAT, (payload) => {
         socket.chatId = payload;
-        console.log(`Usuario con ID  se unió al chat ${payload}`);
     });
 };
 export const handleSendMessage = (socket) => {
     socket.on(events.SEND_MESSAGE, async (payload) => {
         try {
-            console.log("Mensaje de chat recibido:", payload);
             const data = {
                 chatId: payload.chatId,
                 userId: payload.senderUserId,
@@ -20,37 +18,32 @@ export const handleSendMessage = (socket) => {
             };
       
             const savedMessage = await StandardMessage.createStandardMessage(data);
-            socket.broadcast.to(payload.chatId).emit(events.RECEIVE_MESSAGE, savedMessage); // Enviar el mensaje guardado de vuelta a los usuarios
+            socket.broadcast.to(payload.chatId).emit(events.RECEIVE_MESSAGE, savedMessage);
         } catch (error) {
-            console.error("Error al guardar el mensaje:", error);
         }
     });
 };
 export const handleLeaveChat = (socket) => {
     socket.on(events.LEAVE_CHAT, (payload) => {
         socket.leave(payload);
-        console.log(`Salió del chat ${payload}`);
     });
 };
 
 export const handleTypingStatus = (socket) => {
     socket.on(events.TYPING_STATUS, (payload) => {
         socket.broadcast.to(payload.chatId).emit(events.TYPING_STATUS, payload);
-        console.log(`Usuario con ID ${payload.userId} está escribiendo en el chat ${payload.chatId}`);
     });
 };
 
 export const handleUserJoined = (socket) => {
     socket.on(events.USER_JOINED, (payload) => {
         socket.broadcast.to(payload.chatId).emit(events.USER_JOINED, payload);
-        console.log(`Nuevo usuario con ID ${payload.userId} se unió al chat ${payload.chatId}`);
     });
 };
 
 export const handleUserLeft = (socket) => {
     socket.on(events.USER_LEFT, (payload) => {
         socket.broadcast.to(payload.chatId).emit(events.USER_LEFT, payload);
-        console.log(`Usuario con  dejó el chat `);
     });
 };
 
