40e792b6964c94ce196d3bdab3687e67af0e3b9e
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-30 17:09:31 -0300

    Guardado

--- src/Models/ModelTeacher.js
+++ src/Models/ModelTeacher.js
@@ -7,7 +7,6 @@ import { cognitoService } from '../../src/awsconfig/cognitoUtils.js'
 // Crear Teacher
 const createTeacher = async (data = {}) => {
   const timestamp = new Date().toISOString();
-  const teacherId = uuidv4();  
  const { password, confirmPassword, ...dataWithoutPassword } = data;
 
  const isEmailTaken = await emailExists(data.email, UserTable);
@@ -27,7 +26,7 @@ const createTeacher = async (data = {}) => {
     email: data.email,
     profileImageUrl:data.profileImageUrl,
     role: 'teacher',
-    roleId: teacherId,
+    roleId: userId,
     cognitoId:cognitoResult.username, 
     createdAt: timestamp,
     updatedAt: timestamp
@@ -37,7 +36,7 @@ const createTeacher = async (data = {}) => {
    TableName: Table,
    Item: {
      ...dataWithoutPassword,  
-     id: teacherId, 
+     id: userId, 
      userId:userId,
      cognitoId:cognitoResult.username, 
      createdAt: timestamp,
@@ -51,7 +50,7 @@ const createTeacher = async (data = {}) => {
   try {
     await db.send(new PutCommand(teacherParams));
     await db.send(new PutCommand(userParams));
-    return { success: true, id: teacherId };
+    return { success: true, id: userId };
   } catch (error) {
     console.error('Error creating teacher:', error.message);
     return { success: false, message: 'Error creating teacher', error: error.message };
