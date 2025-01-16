124efd996b7c849925683d8afebfc10e9afeb06a
Andres <TuEmail@example.com>
2025-01-16 10:02:18 -0500

    cors para landing Users

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ setupWebSocket(server); // Configura WebSocket
 app.use(cookieParser());
 app.use(
   cors({
-     origin: ["https://a.app.esturio.com","http://localhost:5173", "http://localhost:3000" ],
+     origin: ["https://a.app.esturio.com","http://localhost:5173", "http://localhost:3000", "https://landing.app.esturio.com" ],
   })
 );
 app.use(express.json());
